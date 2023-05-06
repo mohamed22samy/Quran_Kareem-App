42065efd5f9929b5da513110847f824680d59e07
mohamed elshora <mohamedsame0111@gmail.com>
2023-05-06 15:13:37 +0300

    Quran Kareem App

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:quran/constant.dart';
+import 'package:quran/index.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await readJson();
+      await getSettings();
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quran',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const IndexPage(),
+    );
+  }
+}
